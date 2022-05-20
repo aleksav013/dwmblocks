6639d397f20379e44a477d535ae94533b1e9f8a8
Aleksa Vučković <aleksav013@gmail.com>
2022-05-21 01:00:07 +0200

    scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",            "network",                                                      1,      0},
-	{"Cpu: ",       "top -bn 1 | sed '3q;d' | awk {'print $2i\"%\"'}",          	5,      0},
-	{"Mem:",        "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",    	5,	0},
-	{"",            "battery",                                                 	60,     0},
-	{"Brightness:", "echo $(xbacklight -get)%",                                 	0,      6},
-	{"Volume:",     "pamixer --get-volume-human",                              	0,      10},
-	{"",            "date '+%d.%m.%Y. %H:%M'",			         	60,     0},
+	{"",		"network",							1,	0},
+	{"\uf2db ",	"cpu_usage",							5,	0},
+	{"\uf85a ",	"memory_usage",							5,	0},
+	{"\uf241  ",	"battery",							60,	0},
+	{"\uf5df ",	"get_brightness",						0,	6},
+	{"\uf028 ",	"get_volume",							0,	10},
+	{"",		"get_date",							60,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
